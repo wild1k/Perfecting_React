c94898fa5e199bf28fb0449deb3a038c18a6f273
wild1k <cssndrchmbrln@gmail.com>
2020-12-18 14:26:58 -0800

    going over react components and rendering dynamically best practices

--- src/App.js
+++ src/App.js
@@ -5,23 +5,59 @@ import ContactCard from "./Components/ContactCard";
 // import Main from "./Components/MyInfo";
 // import Header from "./Components/Header";
 import ToDo from "./Components/ToDoItems";
-import Joke from "./Components/Joke"
+import Joke from "./Components/Joke";
 
-function App() {
+
+function App(props) {
+  const jokesArray = [
+    {
+      id: 1,
+      question: "Why are elevator jokes the best?",
+      punchLine: "Because they work on every level",
+    },
+ {    id: 2,
+      question: "Did you hear about the restaurant in space?",
+      punchLine: "The food isn't very good but the atmosphere is out of this world",
+    },
+   {
+     id: 3,
+      punchLine: "Money talks: mine always says is goodbye.",
+    },
+]
+
+  const jokeComponent = jokesArray.map(jokes => <Joke key={jokes.id} question={jokes.question} punchLine={jokes.punchLine}/>)
+
   return (
     <>
-    <ContactCard contact={{name:"Mr. Unicorn", phone:"206 349 3894", email:"unicorn@gmail.com", imgUrl:"https://www.sciencenewsforstudents.org/wp-content/uploads/2020/03/1030_onwardunicorns-1-1028x579.png", altImg:"unicorn on rock"}} />
-    <ContactCard contact={{name:"Mr. kitty", phone:"206 349 3243", email:"lotty@gmail.com", imgUrl:"http://www.placekitten.com/200/300", altImg:"cute kitten"}}/>
-<Joke question={"Why are elevator jokes the best?"} punchline={"Because they work on every level"}/>
-<Joke question={"Did you hear about the restaurant in space?"} punchline={"The food isn't very good but the atmosphere is out of this world"}/>
-<Joke punchline={"Money talks: mine always says is goodbye."}/>
- <ToDo/>
- <ToDo/>
- <ToDo/>
- <ToDo/>
- <ToDo/>
+      <ContactCard
+        contact={{
+          name: "Mr. Unicorn",
+          phone: "206 349 3894",
+          email: "unicorn@gmail.com",
+          imgUrl:
+            "https://www.sciencenewsforstudents.org/wp-content/uploads/2020/03/1030_onwardunicorns-1-1028x579.png",
+          altImg: "unicorn on rock",
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Mr. kitty",
+          phone: "206 349 3243",
+          email: "lotty@gmail.com",
+          imgUrl: "http://www.placekitten.com/200/300",
+          altImg: "cute kitten",
+        }}
+      />
+      <>
+      {jokeComponent}
+      </>
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      <ToDo />
     </>
-  );
+  )
 }
 
 export default App;
--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -2,23 +2,11 @@ import React from "react";
 
 export default function Joke(props) {
 
-    const style = {
-        h1: {
-        
-            color: "green",
-            fontSize: 25,
 
-        }
-    }
-    var h1Question = <h1>Question:</h1>
-    console.log(h1Question);
-    if (h1Question !== props) {
-        style.h1Question.textBox =  "none";
-    }
     return(
         <div>
-            <h1 style={style.h1} key={h1Question} >{props.question}</h1>
-            <p>PunchLine: {props.punchline}</p>
+            <h1 style={{display: props.question ? "block" : "none"}} >Question: {props.question}</h1>
+            <p style={{fontSize: !props.question && "35px"}}>PunchLine: {props.punchLine}</p>
         </div>
     )
 }
